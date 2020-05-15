e4b77f026cc24283e09daa9a7d1ba9ba7ab47c12
Stephen Whittaker <stephenwhittaker23@gmail.com>
2020-05-15 15:59:57 +0100

    filter by price, size, breakfast and pets

--- src/Components/RoomsFilter.js
+++ src/Components/RoomsFilter.js
@@ -62,6 +62,62 @@ export default function RoomsFilter({ rooms }) {
             {people}
           </select>
         </div>
+        <div className="form-group">
+          <label htmlFor="price">max room price {price}</label>
+          <input
+            type="range"
+            name="price"
+            min={minPrice}
+            max={maxPrice}
+            id="price"
+            value={price}
+            onChange={handleChange}
+            className="form-control"
+          ></input>
+        </div>
+        <div className="form-group">
+          <label htmlFor="size">room size</label>
+          <div className="size-inputs">
+            <input
+              type="number"
+              name="minSize"
+              id="size"
+              value={minSize}
+              onChange={handleChange}
+              className="size-input"
+            />
+            <input
+              type="number"
+              name="maxSize"
+              id="size"
+              value={maxSize}
+              onChange={handleChange}
+              className="size-input"
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <div className="single-extra">
+            <input
+              type="checkbox"
+              name="breakfast"
+              id="breakfast"
+              checked={breakfast}
+              onChange={handleChange}
+            />
+            <label htmlFor="breakfast">breakfast included</label>
+          </div>
+          <div className="single-extra">
+            <input
+              type="checkbox"
+              name="pets"
+              id="pets"
+              checked={pets}
+              onChange={handleChange}
+            />
+            <label htmlFor="pets">pets allowed</label>
+          </div>
+        </div>
       </form>
     </section>
   );
--- src/context.js
+++ src/context.js
@@ -58,11 +58,10 @@ class RoomProvider extends Component {
 
   handleChange = (event) => {
     const target = event.target;
-    const value = event.type === "checkbox" ? target.checked : target.value;
+    const value = target.type === "checkbox" ? target.checked : target.value;
 
     const name = target.name;
     this.setState({ [name]: value }, this.filterRooms);
-    console.log(name, value);
   };
 
   filterRooms = () => {
@@ -79,12 +78,23 @@ class RoomProvider extends Component {
 
     let tempRooms = [...rooms];
     capacity = Number(capacity);
+    price = Number(price);
     if (type !== "all") {
       tempRooms = tempRooms.filter((room) => room.type === this.state.type);
     }
 
     if (capacity !== 1) {
       tempRooms = tempRooms.filter((room) => room.capacity >= capacity);
+    }
+    tempRooms = tempRooms.filter((room) => room.price <= price);
+    tempRooms = tempRooms.filter(
+      (room) => room.size >= minSize && room.size <= maxSize
+    );
+    if (breakfast) {
+      tempRooms = tempRooms.filter((room) => room.breakfast === true);
+    }
+    if (pets) {
+      tempRooms = tempRooms.filter((room) => room.pets === true);
     }
     this.setState({ sortedRooms: tempRooms });
   };
